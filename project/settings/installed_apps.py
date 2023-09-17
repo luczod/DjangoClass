2d031c2570d67363526b74d5ecea5d55e8c52329
luczod <kzod5611@gmail.com>
2023-09-16 21:35:13 -0300

    module settings created

---
+++ project/settings/installed_apps.py
@@ -0,0 +1,18 @@
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # my apps
+    'recipes',
+    'authors',
+    'tag',
+    'tailwind',
+    'theme',
+    'django_browser_reload'
+
+]
