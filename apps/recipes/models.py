0ad5eaacfb5f946e619049377ebc8fc1c346fcc3
luczod <kzod5611@gmail.com>
2023-09-20 17:54:03 -0300

    handler image pillow

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -1,10 +1,14 @@
+import os
+from PIL import Image
 from tag.models import Tag
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from collections import defaultdict
 from django.utils.text import slugify
 from django.forms import ValidationError
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 
 from django.db.models import F,  Value
@@ -40,7 +44,7 @@ class RecipeManager(models.Manager):
 class Recipe(models.Model):
     # add new personal objects Manager to class
     objects = RecipeManager()
-    title = models.CharField(max_length=65)
+    title = models.CharField(max_length=65, verbose_name=_('Title'))
     description = models.CharField(max_length=165)
     slug = models.SlugField(unique=True)
     preparation_time = models.IntegerField()
@@ -68,12 +72,41 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse('recipes:recipe', args=(self.id,))
 
+    @staticmethod
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+
+        new_height = round((new_width * original_width) / original_height)
+        # shrinking the image and maintaining the aspect ratio
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+
     def save(self, *args, **kwargs):
+        # call pre-save
         if not self.slug:
             slug = f'{slugify(self.title)}'
             self.slug = slug
-        # ao sobreescrever retorna o que deveria tervindo
-        return super().save(*args, **kwargs)
+        # ao sobreescrever retorna o que deveria ter vindo
+        saved = super().save(*args, **kwargs)
+
+        if self.cover:
+            try:
+                self.resize_image(self.cover, 800)
+            except FileNotFoundError:
+                ...
+
+        return saved
+    # call post-save
 
     def clean(self, *args, **kwargs):
         error_messages = defaultdict(list)
---
+++ apps/recipes/signals.py
@@ -0,0 +1,27 @@
+
+import os
+from recipes.models import Recipe
+from django.dispatch import receiver
+from django.db.models.signals import pre_delete, pre_save
+
+
+def delete_cover(instance):
+    try:
+        os.remove(instance.cover.path)
+    except (ValueError, FileNotFoundError) as e:
+        print(e)
+
+
+@receiver(pre_delete, sender=Recipe)
+def recipe_post_signal(sender, instance,  *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk)
+    delete_cover(old_instance)
+
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.filter(pk=instance.pk)
+    is_new_cover = old_instance.cover != instance.cover
+
+    if is_new_cover:
+        delete_cover(old_instance)
